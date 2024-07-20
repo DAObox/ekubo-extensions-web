f4b2ed6c90da8d331b9ed195b18d8a8c7ea6e3ee
supreme2580 <victoromorogbe69@gmail.com>
2024-07-20 16:58:01 +0100

    wip

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 export default function Navigation() {
     return (
         <div className="fixed h-20 backdrop-blur-3xl bg-transparent/10 w-[360px] sm:w-[600px] top-10 rounded-l-full rounded-r-full px-2.5
-        flex flex-col items-center justify-center">
+        flex flex-col items-center justify-center bg-gradient-to-br from-[#41127E] via-transparent to-transparent">
             <div className="flex flex-row items-center justify-between w-full pr-2.5">
-                <Image src={"/logo.svg"} width={50} height={50} alt="logo" />
+                <Link href={"/"}>
+                    <Image src={"/logo.svg"} width={50} height={50} alt="logo" />
+                </Link>
                 <Button className="bg-[#41127E] hover:bg-[#41127E] flex flex-row items-center space-x-1.5">
                     <Image src={"/github-mark-white.svg"} width={25} height={25} alt="logo" />
                     <p className="text-white">Sign in with github</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import TextWriter from "./components/TextWriter";
+import { Input } from "./components/ui/input";
+
 async function getContributors(repoName: string, page = 1) {
   let request = await fetch(
     `https://api.github.com/repos/${repoName}/contributors?per_page=100&page=${page}`,
@@ -37,8 +40,16 @@ export default async function Home() {
   );
 
   return (
-    <div className="bg-[#131216]/95 w-full min-h-screen">
-
+    <div className="bg-[#131216]/95 w-full min-h-screen pt-40 flex flex-col items-center">
+      <div className="max-w-3xl px-5 space-y-5">
+        <h2 className="text-[#41127E] text-center text-5xl sm:text-6xl font-extrabold">
+          <TextWriter words={["Ekubo Community Extensions"]} />
+        </h2>
+        <p className="text-lg text-center text-white">A community curated collection of open-source extensions for Ekubo protocol.</p>
+        <div>
+          <Input placeholder="Search for extensions" className="bg-transparent focus-visible:ring-0 focus-visible:border-none" />
+        </div>
+      </div>
     </div>
   );
 }
