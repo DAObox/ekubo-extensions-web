8ffb29a76889793c180c750ba5d67e1366c0a260
supreme2580 <victoromorogbe69@gmail.com>
2024-07-20 16:15:24 +0100

    make header responsive

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 
 export default function Navigation() {
     return (
-        <div className="fixed h-20 backdrop-blur-3xl bg-transparent/10 w-[600px] top-10 rounded-l-full rounded-r-full px-2.5
+        <div className="fixed h-20 backdrop-blur-3xl bg-transparent/10 w-[360px] sm:w-[600px] top-10 rounded-l-full rounded-r-full px-2.5
         flex flex-col items-center justify-center">
             <div className="flex flex-row items-center justify-between w-full pr-2.5">
                 <Image src={"/logo.svg"} width={50} height={50} alt="logo" />
