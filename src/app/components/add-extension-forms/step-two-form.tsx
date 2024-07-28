74d081bb872bc3c5411b216b7159156467f6596b
supreme2580 <victoromorogbe69@gmail.com>
2024-07-28 15:41:33 +0100

    wip

--- src/app/components/add-extension-forms/step-two-form.tsx
+++ src/app/components/add-extension-forms/step-two-form.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { Button } from "../../components/ui/button"
 import {
     Form,
     FormControl,
@@ -17,17 +16,29 @@ import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { useRouter } from "next/navigation"
 
+import { Button } from "../../components/ui/button"
+
+import { useState } from "react"
+
 export default function StepTwoForm() {
     const router = useRouter()
     const formSchema = z.object({
         repo: z.string().url({
             message: "Please enter a valid github URL.",
         }),
+        deployment_address: z.string().min(2, {
+            message: "Deployment address must be at least 2 characters.",
+        }),
+        network: z.string().min(2, {
+            message: "Please select a network",
+        })
     })
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
             repo: "",
+            deployment_address: "",
+            network: "",
         },
     })
     function onSubmit(values: z.infer<typeof formSchema>) {
@@ -53,6 +64,21 @@ export default function StepTwoForm() {
                         </FormItem>
                     )}
                 />
+                <FormField
+                    control={form.control}
+                    name="deployment_address"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel className="text-white">Deployment address</FormLabel>
+                            <FormControl>
+                                <Input placeholder="0x1234..." {...field} className="w-full text-white bg-transparent focus-visible:ring-0" />
+                            </FormControl>
+                            <FormDescription className="text-white">
+                                Enter the address of your deployed hook.
+                            </FormDescription>
+                            <FormMessage className="text-red-700" />
+                        </FormItem>
+                    )} />
                 <Button type="submit" className="h-8 text-white bg-[#41127E] hover:bg-transparent hover:border-2 border-[#41127E]">Next</Button>
             </form>
         </Form>
